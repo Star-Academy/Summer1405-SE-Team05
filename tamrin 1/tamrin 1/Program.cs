2c8b1a054fc242c1a03db3c7318a8ae8c93094c7
MsJ <jeihanimohammadsadegh@gmail.com>
2026-07-28 17:04:36 +0330

    full project

---
+++ tamrin 1/tamrin 1/Program.cs
@@ -0,0 +1,42 @@
+﻿List<String> list = new List<String>();
+int current = -1;
+while (true)
+{
+    String s = Console.ReadLine().Trim().ToLower();
+    
+    if (s.Equals("exit"))
+    {
+        break;
+    }else if (s.StartsWith("search"))
+    {
+        list.Add(s.Substring("search".Length));
+        current++;
+    }else if (s.StartsWith("back"))
+    {
+        if (current > 0) current--;
+    }else if (s.StartsWith("forward"))
+    {
+        if(current < list.Count-1)current++;
+    }else if (s.StartsWith("current"))
+    {
+        if (current == -1) Console.WriteLine("it s empty");
+        else Console.WriteLine(list[current]);
+    }else if (s.StartsWith("stats"))
+    {
+        var result = list.GroupBy(w => w).OrderByDescending(x => x.Count());
+        int count = 0;
+        foreach (var item in result)
+        {
+            Console.WriteLine(item.Key);
+            count++;
+            if(count == 3)break;
+        }
+    }else if (s.StartsWith("unique"))
+    {
+        var result = list.GroupBy(w => w);
+        foreach (var item in result)
+        {
+            if(item.Count() == 1) Console.WriteLine(item.Key);
+        }
+    }
+}
